914da0835d85a336fd1e1dba1aeae1f4d60120b3
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-10-14 08:03:47 +0200

    Add `npm run bootstrap`

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -2,7 +2,6 @@
 
 const {cd, ls, exec, pwd} = require('shelljs');
 const originalDirectory = pwd();
-const npm = `${__dirname}/../node_modules/.bin/npm`;
 
 const packages = ls(`${__dirname}/../packages/*`).map(location => ({
   location,
@@ -10,18 +9,20 @@ const packages = ls(`${__dirname}/../packages/*`).map(location => ({
 }));
 
 packages.forEach(({location}) => {
-  cd(location);
-  exec(`${npm} link`);
+  cd(`"${location}"`);
+  exec('npm link');
 });
 
 const isOurPackage = /^parametric-svg-/;
-packages.forEach(({location, manifest: {dependencies, devDependencies}}) => {
-  cd(location);
-  exec(`${npm} install`);
+packages.forEach((
+  {location, manifest: {dependencies = {}, devDependencies = {}}}
+) => {
+  cd(`"${location}"`);
+  exec('npm install');
 
   Object.keys(dependencies).concat(Object.keys(devDependencies))
     .filter(name => isOurPackage.test(name))
-    .forEach(name => exec(`${npm} link ${name}`));
+    .forEach(name => exec(`npm link ${name}`));
 });
 
 cd(originalDirectory);
