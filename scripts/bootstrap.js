33928b710be8e812cd449de72f81bb40baa76eb3
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-10-14 12:37:20 +0200

    Be more helpful

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -1,24 +1,35 @@
 #! /usr/env babel-node
+/* eslint-disable no-console */
 
 const {ls} = require('shelljs');
 const {execSync} = require('child_process');
+
+const exec = (...args) => {
+  process.stdout.write(execSync(...args));
+};
 
 const packages = ls(`${__dirname}/../packages/*`).map(cwd => ({
   cwd,
   manifest: require(`${cwd}/package.json`),
 }));
 
+console.log('\nCreating npm links…');
 packages.forEach(({cwd}) => {
-  execSync('npm link', {cwd});
+  exec('npm link', {cwd});
 });
+console.log('…done.');
 
 const isOurPackage = /^parametric-svg-/;
 packages.forEach((
-  {cwd, manifest: {dependencies = {}, devDependencies = {}}}
+  {cwd, manifest: {dependencies = {}, devDependencies = {}, name}}
 ) => {
-  execSync('npm install', {cwd});
+  console.log(`\nInstalling and linking dependendies for \`${name}\`…`);
+
+  exec('npm install', {cwd});
 
   Object.keys(dependencies).concat(Object.keys(devDependencies))
-    .filter(name => isOurPackage.test(name))
-    .forEach(name => execSync(`npm link ${name}`, {cwd}));
+    .filter(module => isOurPackage.test(module))
+    .forEach(module => exec(`npm link ${module}`, {cwd}));
+
+  console.log('…done.');
 });
