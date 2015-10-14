8c7c9dce0891fe965656c4c5ca4792cbc913d25a
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-10-14 22:27:28 +0200

    Update test titles

--- packages/parametric-svg-expression-to-mathjs/test.js
+++ packages/parametric-svg-expression-to-mathjs/test.js
@@ -5,127 +5,127 @@ spec('Mathematical expressions', (test) => {
   test.equal(
     parse('2 * (3 + 4)'),
     'string(2 * (3 + 4))',
-    'Parentheses'
+    'parentheses'
   );
 
   test.equal(
     parse('max(2, 1, 5)'),
     'string(max(2, 1, 5))',
-    'Parameter separator'
+    'parameter separator'
   );
 
   test.equal(
     parse('4 + 5'),
     'string(4 + 5)',
-    'Add'
+    'add'
   );
 
   test.equal(
     parse('7 - 3'),
     'string(7 - 3)',
-    'Subtract'
+    'subtract'
   );
 
   test.equal(
     parse('-4'),
     'string(-4)',
-    'Unary minus'
+    'unary minus'
   );
 
   test.equal(
     parse('2 * 3'),
     'string(2 * 3)',
-    'Multiply'
+    'multiply'
   );
 
   test.equal(
     parse('6 / 2'),
     'string(6 / 2)',
-    'Divide'
+    'divide'
   );
 
   test.equal(
     parse('8 % 3'),
     'string(8 % 3)',
-    'Modulus'
+    'modulus'
   );
 
   test.equal(
     parse('2 ^ 3'),
     'string(2 ^ 3)',
-    'Power'
+    'power'
   );
 
   test.equal(
     parse('5!'),
     'string(5!)',
-    'Factorial'
+    'factorial'
   );
 
   test.equal(
     parse('true and false ? 1 : 0'),
     'string(true and false ? 1 : 0)',
-    'Logical and'
+    'logical and'
   );
 
   test.equal(
     parse('not true ? 1 : 0'),
     'string(not true ? 1 : 0)',
-    'Logical not'
+    'logical not'
   );
 
   test.equal(
     parse('true or false ? 1 : 0'),
     'string(true or false ? 1 : 0)',
-    'Logical or'
+    'logical or'
   );
 
   test.equal(
     parse('true xor true ? 1 : 0'),
     'string(true xor true ? 1 : 0)',
-    'Logical xor'
+    'logical xor'
   );
 
   test.equal(
     parse('false ? 1 : -1'),
     'string(false ? 1 : -1)',
-    'Conditional expression'
+    'conditional expression'
   );
 
   test.equal(
     parse('2 == 4 - 2 ? 1 : 0'),
     'string(2 == 4 - 2 ? 1 : 0)',
-    'Equal'
+    'equal'
   );
 
   test.equal(
     parse('2 != 3 ? 1 : 0'),
     'string(2 != 3 ? 1 : 0)',
-    'Unequal'
+    'unequal'
   );
 
   test.equal(
     parse('2 < 3 ? 1 : 0'),
     'string(2 < 3 ? 1 : 0)',
-    'Smaller'
+    'smaller'
   );
 
   test.equal(
     parse('2 > 3 ? 1 : 0'),
     'string(2 > 3 ? 1 : 0)',
-    'Larger'
+    'larger'
   );
 
   test.equal(
     parse('4 <= 3 ? 1 : 0'),
     'string(4 <= 3 ? 1 : 0)',
-    'Smallereq'
+    'smallereq'
   );
 
   test.equal(
     parse('2 + 4 >= 6 ? 1 : 0'),
     'string(2 + 4 >= 6 ? 1 : 0)',
-    'Largereq'
+    'largereq'
   );
 
   test.end();
@@ -135,25 +135,25 @@ spec('Simple strings', (test) => {
   test.equal(
     parse('`green`'),
     '"green"',
-    'Very simple'
+    'very simple'
   );
 
   test.equal(
     parse('`"\'\\``'),
     '"\\"\'`"',
-    'Quotes'
+    'quotes'
   );
 
   test.equal(
     parse('`\\\`\$`'),
     '"\\`$"',
-    'Escapes'
+    'escapes'
   );
 
   test.equal(
     parse('`ab\n  cd\t  ef`'),
     '"ab\\n  cd\\t  ef"',
-    'Whitespace'
+    'whitespace'
   );
 
   test.end();
