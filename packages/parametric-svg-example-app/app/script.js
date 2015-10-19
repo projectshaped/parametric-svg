8ca3e62fa4b62ae2ac79e61e062107928e58a2ef
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-10-19 22:13:31 +0200

    Control `a` with a slider

--- packages/parametric-svg-example-app/app/script.js
+++ packages/parametric-svg-example-app/app/script.js
@@ -2,9 +2,30 @@ const getSvg = new XMLHttpRequest();
 
 getSvg.onload = function onload() {
   const parametricSvg = document.createElement('parametric-svg');
-  parametricSvg.setAttribute('a', '200');
+
   parametricSvg.innerHTML = this.responseText;
   document.body.appendChild(parametricSvg);
+
+  function updateFunction(range, display, parameter) {
+    return function update() {
+      const value = range.value;
+      display.textContent = value;
+      parametricSvg.setAttribute(parameter, value);
+    };
+  }
+
+  ['a'].forEach(function setStuff(variable) {
+    const range = document.getElementById(variable);
+
+    const update = updateFunction(
+      range,
+      document.getElementById(variable + '-display'),
+      variable
+    );
+
+    update();
+    range.addEventListener('input', update);
+  });
 };
 
 getSvg.open('get', 'beam.parametric.svg', true);
