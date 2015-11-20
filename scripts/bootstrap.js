1bff5c2a56e24271f41dfb58c33f9d35e8fc3a89
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-11-20 22:12:04 +0100

    Allow scoped packages in `npm run bootstrap`

--- scripts/bootstrap.js
+++ scripts/bootstrap.js
@@ -1,13 +1,16 @@
 #! /usr/env babel-node
 
-const {ln} = require('shelljs');
+const { ln, mkdir } = require('shelljs');
+const { resolve } = require('path');
 const packages = require('./utilities/packages');
 
 const projectRoot = `${__dirname}/..`;
 
 console.log('\nLinking our packages…');
-packages.forEach(({cwd, manifest: {name}}) => {
-  ln('-sf', `${cwd}`, `${projectRoot}/node_modules/${name}`);
+packages.forEach(({ cwd, manifest: { name } }) => {
+  const target = `${projectRoot}/node_modules/${name}`;
+  mkdir('-p', resolve(target, '..'));
+  ln('-sf', `${cwd}`, target);
 });
 console.log('…done.');
 
