b033f2c4102002bc07c1c31680dfab407f416ae6
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-11-20 21:48:17 +0100

    Update global dependencies first

--- scripts/sync-dependencies.js
+++ scripts/sync-dependencies.js
@@ -1,15 +1,28 @@
 #! /usr/bin/env babel-node
 
 const fs = require('fs');
+const sortKeys = require('sort-keys');
 
 const packages = require('./utilities/packages');
 
 const globalManifest = require('../package.json');
-const dependencies = globalManifest.dependencies;
+const originalDependencies = globalManifest.dependencies;
 
 const serialize = (value) => JSON.stringify(value, null, 2) + '\n';
 
-// Update versions in local manifests.
+// Pull local dependencies into global manifest.
+const dependencies = sortKeys(packages.reduce((target, { manifest }) => {
+  return Object.assign({}, manifest.dependencies, target);
+}, originalDependencies));
+
+const newGlobalManifest = Object.assign({}, globalManifest, { dependencies });
+
+fs.writeFileSync(
+  `${__dirname}/../package.json`,
+  serialize(newGlobalManifest)
+);
+
+// Push global versions to local manifests.
 packages.forEach(({ cwd, manifest }) => {
   if (!manifest.dependencies) return;
 
@@ -24,17 +37,3 @@ packages.forEach(({ cwd, manifest }) => {
     serialize(newManifest)
   );
 });
-
-// Update global dependencies.
-const newDependencies = packages.reduce((target, { manifest }) => {
-  return Object.assign(target, manifest.dependencies);
-}, Object.assign({}, dependencies));
-
-const newGlobalManifest = Object.assign({}, globalManifest, {
-  dependencies: newDependencies,
-});
-
-fs.writeFileSync(
-  `${__dirname}/../package.json`,
-  serialize(newGlobalManifest)
-);
