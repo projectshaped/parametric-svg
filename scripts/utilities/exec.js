ce6885db2427d6f2e630b6da01acf2216a9b5a86
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-11-21 21:51:27 +0100

    Fix typo

--- scripts/utilities/exec.js
+++ scripts/utilities/exec.js
@@ -1,7 +1,7 @@
 const execSync = require('child_process').execSync;
 
 module.exports = function exec() {
-  const args = Array.prototype.slice.call(args);
+  const args = Array.prototype.slice.call(arguments);
 
   console.log(`${args[1] && args[1].cwd || ''}$ ${args[0]}`);
   process.stdout.write(execSync(...args));
