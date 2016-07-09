1656a470a4fd487075aa5e621f1f5fb58c94de79
Tomek <tomek@lystable.com>
2016-07-09 21:49:25 +0200

    Fix script syntax

--- scripts/utilities/exec.js
+++ scripts/utilities/exec.js
@@ -4,5 +4,5 @@ module.exports = function exec() {
   const args = Array.prototype.slice.call(arguments);
 
   console.log(`${args[1] && args[1].cwd || ''}$ ${args[0]}`);
-  process.stdout.write(execSync(...args));
+  process.stdout.write(execSync.apply(null, args));
 };
